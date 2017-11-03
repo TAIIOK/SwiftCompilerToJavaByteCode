389ebe5c6eae37c522c2bdc940ff8f745839f9ea
Roman <bolobolbfmv@gmail.com>
2017-11-03 17:21:54 +0300

    test

--- build/test.swift
+++ build/test.swift
@@ -1,25 +1,12 @@
-var a = 1
-var b = 4
-if (a < b){
-    a = a + 1
-}
-print(a)
 
-if (b > a){
-    b = a + b
-}
-else{
-     b = b - 1
-}
-print(b)
+func s() {
 
-if (a != b){
-    print("wow")
-    if(a == b ){
-    print("asdasd")
-    }
-} else if (a == b){
+var sa = 0
 
-    print("wow2")
+for sa in 0 ... 10{
+    print(sa)
 }
 
+}
+
+s()
